8dd6859a8d61338b25cf1c8a088607952df751c7
James Ball <jamie.j.ball@gmail.com>
2022-06-14 08:41:00 -0700

    consistency with naming

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import './DayListItem.scss';
 
-export default function DayListItem(prop) {
+export default function DayListItem(props) {
   const formatSpots = (spots) => {
     if (spots === 0) {
       return 'no spots remaining';
@@ -10,13 +10,14 @@ export default function DayListItem(prop) {
     return spots === 1 ? '1 spot remaining' : `${spots} spots remaining`;
   };
   const dayClass = classNames('day-list__item', {
-    'day-list__item--selected': prop.selected,
-    'day-list__item--full': prop.spots === 0,
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0,
   });
+
   return (
-    <li onClick={() => prop.setDay(prop.name)} className={dayClass}>
-      <h2 className="text--regular">{prop.name}</h2>
-      <h3 className="text--light">{formatSpots(prop.spots)}</h3>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
