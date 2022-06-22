9cf9cc2e60c9a04214f98e444dccacdec8af36d3
James Ball <jamie.j.ball@gmail.com>
2022-06-21 18:21:58 -0700

    added a property checker

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
-import './InterviewerList.scss';
+import '../styles/InterviewerList.scss';
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+const InterviewerList = (props) => {
   const { interviewers, value, onChange } = props;
 
   const interviewerArray = interviewers.map((interviewer) => {
@@ -22,4 +23,9 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{interviewerArray}</ul>
     </section>
   );
-}
+};
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
